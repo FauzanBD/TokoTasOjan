d03b958e729557cdb51c37f60e221cdea8936be3
FauzanBD <77427821+FauzanBD@users.noreply.github.com>
2021-01-15 00:52:15 +0700

    Add files via upload

---
+++ Fauzan07210_Allobjctrl.java
@@ -0,0 +1,7 @@
+package View;
+import Controller.AdminController;
+import Controller.PembeliController;
+public class Fauzan07210_Allobjctrl {
+    public static AdminController admin = new AdminController();
+    public static PembeliController pembeli = new PembeliController();
+}
---
+++ TasEntity.java
@@ -0,0 +1,6 @@
+package Entity;
+public class TasEntity {
+      public static String tas []  = {"Eiger   - Rp.800.000", 
+                                      "Consina - Rp.600.000", 
+                                      "Arei    - Rp.400.000"};
+}
